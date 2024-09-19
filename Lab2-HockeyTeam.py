57be3cc4dff339138b30de36989db05344a30624
w0425777 <w0425777@nscc.ca>
2024-09-19 11:42:53 -0300

    Finished

--- Lab2-HockeyTeam.py
+++ Lab2-HockeyTeam.py
@@ -1,24 +1,22 @@
-# LAB 2 - HOCKEY TEAM
-#Write a program that will ask the user to enter the name of a hockey team, how many wins the team has and 
-# how many losses #the team has.
-
-#The program should calculate and display a single string output containing the team name, it's win-loss 
-# ratio and the win #percentage formatted to 4 decimal places.
-
-#Ex: The Calgary Flames have 10 wins and 5 losses, with a win percentage of 0.6667.
-
-#Purpose/Concepts: Input and output to screen, string concatentation, string formatting, datatype casting, 
-# simple math operations
+"""
+Student Name: Katherine Tucker   
+Program Title: IT Programming 
+Description: Lab2-HockeyTeam   
+"""
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-    Test
+    teamName = input("What is your favourite hockey team? ")
 
+    win = input("How many wins does this team have? ")
 
+    loss = input("How many losses does this team have? ")
+    #Make sure to convert input strings into integers
 
+    finalTotal = (int(win) / (int(win) + int(loss)))
+    #Converted ^
 
-
-
+    print("The {0} have {1} wins and {2} losses, with a win percentage of {3:.4}".format(teamName,win,loss,finalTotal))
 
     # YOUR CODE ENDS HERE
 
